24a6bc46e9332f33aa7278a7d40e66b1dd04f070
rabhi <rabhi.benyoucef134@gmail.com>
2018-08-06 14:44:41 +0100

    modification contact

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
 contact.php
 
 <a href="#">Add URL Rachid</a>
+<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia veritatis maxime est, assumenda, ea voluptate non id impedit rerum aperiam aut. A id minima sed aliquid dolores nulla non aut.</h1>
