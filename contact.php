731fbfcb3a798b24051fde310821416ca9eaf9fe
[Alaa] <[sahraoui.alaaeddine@gmail.com]>
2018-08-06 14:57:22 +0100

    alaa commit

--- contact.php
+++ contact.php
@@ -2,3 +2,6 @@ contact.php
 
 <a href="#">Add URL Rachid</a>
 <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia veritatis maxime est, assumenda, ea voluptate non id impedit rerum aperiam aut. A id minima sed aliquid dolores nulla non aut.</h1>
+<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio rem maiores necessitatibus assumenda quo, voluptates non eum consequatur, cumque eligendi possimus esse culpa. Ipsam veniam nesciunt repellat expedita, voluptatibus quam..</h1>
+<input type="text">
+<input type="password">
